e7a57f8859f6037ad8536cbefc0add4f7f5c46db
rajchauhan2816 <rajchauhan2816@gmail.com>
2020-07-14 19:24:06 +0530

    Fixes Volume

--- packages/MicroService/src/app.service.ts
+++ packages/MicroService/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(data: string): string {
-    return `Hello ${data}`;
+    return `Hello World ${data}`;
   }
 }    
